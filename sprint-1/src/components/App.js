6e92bc5727a2651ee2172cdbc489ac0a0e6f9291
Lili Fernandez <lilibetfdez@gmail.com>
2022-07-26 16:10:31 +0200

    update image path

--- sprint-1/src/components/App.js
+++ sprint-1/src/components/App.js
@@ -4,39 +4,32 @@ function App() {
   return (
     <div className="page">
       <header>
-        <h1 className="header__title">Juego del ahorcado</h1>
-      </header>
+        <h1 className="header__title"> Juego del ahorcado </h1>{" "}
+      </header>{" "}
       <main className="main">
         <section>
           <div className="solution">
-            <h2 className="title">Solución:</h2>
+            <h2 className="title"> Solución: </h2>{" "}
             <ul className="letters">
-              <li className="letter">k</li>
-              <li className="letter">a</li>
-              <li className="letter"></li>
-              <li className="letter">a</li>
-              <li className="letter">k</li>
-              <li className="letter">r</li>
-              <li className="letter"></li>
-              <li className="letter">k</li>
-              <li className="letter">e</li>
-              <li className="letter">r</li>
-            </ul>
-          </div>
+              <li className="letter"> k </li> <li className="letter"> a </li>{" "}
+              <li className="letter"> </li> <li className="letter"> a </li>{" "}
+              <li className="letter"> k </li> <li className="letter"> r </li>{" "}
+              <li className="letter"> </li> <li className="letter"> k </li>{" "}
+              <li className="letter"> e </li> <li className="letter"> r </li>{" "}
+            </ul>{" "}
+          </div>{" "}
           <div className="error">
-            <h2 className="title">Letras falladas:</h2>
+            <h2 className="title"> Letras falladas: </h2>{" "}
             <ul className="letters">
-              <li className="letter">f</li>
-              <li className="letter">q</li>
-              <li className="letter">h</li>
-              <li className="letter">p</li>
-              <li className="letter">x</li>
-            </ul>
-          </div>
+              <li className="letter"> f </li> <li className="letter"> q </li>{" "}
+              <li className="letter"> h </li> <li className="letter"> p </li>{" "}
+              <li className="letter"> x </li>{" "}
+            </ul>{" "}
+          </div>{" "}
           <form className="form">
-            <label className="title" for="last-letter">
+            <label className="title" htmlFor="last-letter">
               Escribe una letra:
-            </label>
+            </label>{" "}
             <input
               autocomplete="off"
               className="form__input"
@@ -45,24 +38,24 @@ function App() {
               name="last-letter"
               id="last-letter"
             />
-          </form>
-        </section>
+          </form>{" "}
+        </section>{" "}
         <section className="dummy error-5">
-          <span className="error-13 eye"></span>
-          <span className="error-12 eye"></span>
-          <span className="error-11 line"></span>
-          <span className="error-10 line"></span>
-          <span className="error-9 line"></span>
-          <span className="error-8 line"></span>
-          <span className="error-7 line"></span>
-          <span className="error-6 head"></span>
-          <span className="error-5 line"></span>
-          <span className="error-4 line"></span>
-          <span className="error-3 line"></span>
-          <span className="error-2 line"></span>
-          <span className="error-1 line"></span>
-        </section>
-      </main>
+          <span className="error-13 eye"> </span>{" "}
+          <span className="error-12 eye"> </span>{" "}
+          <span className="error-11 line"> </span>{" "}
+          <span className="error-10 line"> </span>{" "}
+          <span className="error-9 line"> </span>{" "}
+          <span className="error-8 line"> </span>{" "}
+          <span className="error-7 line"> </span>{" "}
+          <span className="error-6 head"> </span>{" "}
+          <span className="error-5 line"> </span>{" "}
+          <span className="error-4 line"> </span>{" "}
+          <span className="error-3 line"> </span>{" "}
+          <span className="error-2 line"> </span>{" "}
+          <span className="error-1 line"> </span>{" "}
+        </section>{" "}
+      </main>{" "}
     </div>
   );
 }
